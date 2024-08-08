4c1381c3402ef0762d27578fa88bdea169403890
nazi <nazipaeez777@gmail.com>
2024-08-09 00:33:17 +0330

    Create message bar in chat

--- src/components/Chat.js
+++ src/components/Chat.js
@@ -1,9 +1,12 @@
 import { TbSearch } from "react-icons/tb";
 import { FiPhone } from "react-icons/fi";
 import { BsThreeDotsVertical } from "react-icons/bs";
+import { BsEmojiSmile } from "react-icons/bs";
+import { MdLocationOn } from "react-icons/md";
+import { GrAttachment } from "react-icons/gr";
+import { BsFillSendFill } from "react-icons/bs";
 
 import Image from "next/image";
-
 
 export default function Chat() {
   return (
@@ -21,17 +24,42 @@ export default function Chat() {
               last seen recently
             </span>
           </div>
-      
         </div>
         <div className="flex items-center gap-x-8 cursor-pointer text-2xl text-zinc-400">
-           <FiPhone />
-           <TbSearch/>
-           <BsThreeDotsVertical/>
-          </div>
+          <FiPhone />
+          <TbSearch />
+          <BsThreeDotsVertical />
+        </div>
       </div>
-      <div className="h-[90vh]">
-        <Image src={'/images/bg.png'} alt='bg' width={1000}  height={1000}
-        className="w-full h-full"/>
+      <div className="relative h-[90vh]">
+        <Image
+          src={"/images/bg.png"}
+          alt="bg"
+          width={1000}
+          height={1000}
+          className="w-full h-full"
+        />
+        <div className=" flex justify-between items-center px-6 bg-[#bdbcbc] w-4/5 rounded-2xl h-14 absolute bottom-6 left-1/2 -translate-x-1/2 gap-x-4">
+          <BsEmojiSmile className="text-3xl cursor-pointer" />
+          <input type="text" className="flex-1 h-full bg-transparent outline-none" />
+          <div className="flex items-center gap-x-5 text-2xl">
+            <div className="">
+              <label htmlFor="location">
+                <MdLocationOn className="text-3xl cursor-pointer mt-8" />
+              </label>
+              <input
+                id="location"
+                type="file"
+                className="invisible w-0 h-0 opacity-0 "
+              />
+            </div>
+
+            <GrAttachment className="cursor-pointer" />
+            <button className="flex justify-center items-center bg-purple-600 rounded-full p-2">
+              <BsFillSendFill className="text-white" />
+            </button>
+          </div>
+        </div>
       </div>
     </div>
   );
